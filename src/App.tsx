eb233d23a9fa48c655d3ce2056eb03ae032b4e3c
Nikita Vozisov <Nikita_Vozisov@epam.com>
2019-11-19 23:59:44 +0300

    [add] webpack aliases

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import Comp from "components/Comp";
+import { Button } from "@material-ui/core";
+
 export default function App() {
   const a: Number = 5;
-  return <>Hello world! {a}</>;
+  return (
+    <>
+      Hello world! {a} <Comp />
+      <Button variant="contained" color="primary">
+        Hello
+      </Button>
+    </>
+  );
 }
