51628401af4eaa2fbe2c45dc6b465f2b493d415a
Nikita Vozisov <Nikita_Vozisov@epam.com>
2019-11-24 23:04:57 +0300

    [add] login with hooks

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,4 +1,4 @@
-import React, { SyntheticEvent } from "react";
+import React, { SyntheticEvent, useState } from "react";
 import { connect } from "react-redux";
 import { TextField } from "@material-ui/core";
 
@@ -23,54 +23,47 @@ const mapDispatchToProps = (dispatch: Function) => {
   };
 };
 
-class Login extends React.Component<any, any> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      login: "",
-      password: ""
-    };
-  }
+const Login = (props: any) => {
+  const [login, changeLogin] = useState("");
+  const [password, changePassword] = useState("");
 
-  onLoginChange = (event: SyntheticEvent): void => {
-    this.setState({ login: (event.target as HTMLInputElement).value });
+  const onLoginChange = (event: SyntheticEvent): void => {
+    changeLogin((event.target as HTMLInputElement).value);
   };
 
-  onPasswordChange = (event: SyntheticEvent): void => {
-    this.setState({ password: (event.target as HTMLInputElement).value });
+  const onPasswordChange = (event: SyntheticEvent): void => {
+    changePassword((event.target as HTMLInputElement).value);
   };
 
-  formSubmit = (event: SyntheticEvent): any => {
+  const formSubmit = (event: SyntheticEvent): any => {
     event.preventDefault();
-    this.props.authenticate(this.state.login, this.state.password);
+    props.authenticate(login, password);
   };
 
-  render() {
-    return (
-      <div className="align-center form-wrapper">
-        {this.props.isLoading ? (
-          <OCSLoader />
-        ) : (
-          <form onSubmit={(event: SyntheticEvent) => this.formSubmit(event)}>
-            <div className="login-text margin-top">Вход в систему</div>
-            <TextField
-              className="margin-top"
-              label="Логин"
-              onChange={this.onLoginChange}
-            />
-            <TextField
-              className="margin-top"
-              label="Пароль"
-              onChange={this.onPasswordChange}
-            />
-            <OCSButton className="login-btn" type="submit">
-              Войти
-            </OCSButton>
-          </form>
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="align-center form-wrapper">
+      {props.isLoading ? (
+        <OCSLoader />
+      ) : (
+        <form onSubmit={(event: SyntheticEvent) => formSubmit(event)}>
+          <div className="login-text margin-top">Вход в систему</div>
+          <TextField
+            className="margin-top"
+            label="Логин"
+            onChange={onLoginChange}
+          />
+          <TextField
+            className="margin-top"
+            label="Пароль"
+            onChange={onPasswordChange}
+          />
+          <OCSButton className="login-btn" type="submit">
+            Войти
+          </OCSButton>
+        </form>
+      )}
+    </div>
+  );
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
